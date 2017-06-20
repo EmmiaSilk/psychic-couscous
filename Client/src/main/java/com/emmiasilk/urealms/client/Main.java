8fcd2e904b74ea76e5cb39b8c368f989ae711bc7
Katie Macke <katie@flutterflies.net>
2017-06-20 01:30:18 -0400

    gradle inheritance

---
+++ Client/src/main/java/com/emmiasilk/urealms/client/Main.java
@@ -0,0 +1,10 @@
+package com.emmiasilk.urealms.client;
+
+import com.emmiasilk.urealms.core.logging.Logging;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logging.logInfo("Starting Client...");
+    }
+}
